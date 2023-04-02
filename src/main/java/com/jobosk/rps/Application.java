1202a10a4a250598b32dad87526b079538d7a3b0
jobosk <jfbosca+github@gmail.com>
2023-04-02 22:46:30 +0200

    Disable CORS to allow queries from the local front-end.

--- src/main/java/com/jobosk/rps/Application.java
+++ src/main/java/com/jobosk/rps/Application.java
@@ -3,6 +3,9 @@ package com.jobosk.rps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -10,5 +13,22 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Allows cross origin requests for swagger-ui
+     */
+    @Bean
+    public WebMvcConfigurer disableCors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                ;
+            }
+        };
     }
 }
